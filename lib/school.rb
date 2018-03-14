40afce025d0e7a3d86b7f4fd516c5901870029f0
victoria-huang <vctrah@gmail.com>
2018-03-14 10:59:03 -0400

    add class and methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+        @roster[grade] = []
+    end
+
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.update(@roster) {|grade, students| students.sort}
+  end
+end
