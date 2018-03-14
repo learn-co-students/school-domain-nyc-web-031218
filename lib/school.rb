d874a402eded52953d5dbeada7cd2e375932d283
Eric Kim <hyeokjungkim@gmail.com>
2018-03-14 10:28:08 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade].is_a?(Array)
+      roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      names.sort!
+    end
+  end
+end
