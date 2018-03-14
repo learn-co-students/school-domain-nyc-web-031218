0280ecc43ef10fb0ce094d9179ca67127f312aa0
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-03-14 13:57:10 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  attr_accessor :roster, :grade
+
+  def add_student(name, grade)
+    if roster[grade]
+        roster[grade] << name
+    else
+      roster[grade] = []
+      roster[grade] << name
+    end
+  end
+
+  def grade (grade)
+    @grade = roster[grade]
+  end
+
+  def sort
+    roster.each do |key, value|
+      roster[key] = roster[key].sort
+    end
+  end
+
+end
