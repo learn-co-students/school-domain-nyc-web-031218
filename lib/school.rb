38142a34f9a4b32bb566b5a51c295371d31f0cb8
github username <github email address>
2018-03-15 10:46:33 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require "pry"
+
+class School
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new{|h,k| h[k] = []}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    #self.roster[grade]=[]
+    self.roster[grade] << student
+  end
+
+  def grade(year)
+    @roster[year]
+  end
+
+  def sort
+    @roster.each { |grade, students| roster[grade] = students.sort}
+  end
+
+end
