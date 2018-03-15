9a2c9edcb2a80aecb79596b6f19277c745d1045a
Helen Liutongco <liutongcoh@gmail.com>
2018-03-14 20:51:35 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+
+
+class School
+  attr_reader :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+
+    roster.each do |key, array|
+      new_hash[key] = array.sort
+    end
+    new_hash
+  end
+end
